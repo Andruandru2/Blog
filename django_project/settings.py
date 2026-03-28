ae3940c6cf7e6b9e9425bc6333bcbb69d447294f
Andruandru2 <146512498+Andruandru2@users.noreply.github.com>
2026-03-28 11:25:23 +0530

    Update settings.py

--- django_project/settings.py
+++ django_project/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-+vsyo5wz-f_fp0odq_bzez!@po@5m2o4-@lfmqnrha!b(*sgsy
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['django_project.onrender.com']
 
 
 # Application definition
