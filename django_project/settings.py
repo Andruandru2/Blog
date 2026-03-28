95572f5ccf5fcc1650a6d8b14e676b14829660b6
unknown <andruandru3002@gmail.com>
2026-03-28 12:56:25 +0530

    fix media serving

--- django_project/settings.py
+++ django_project/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-+vsyo5wz-f_fp0odq_bzez!@po@5m2o4-@lfmqnrha!b(*sgsy
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['blog-6g0d.onrender.com']
 
 
 # Application definition
@@ -123,7 +123,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
